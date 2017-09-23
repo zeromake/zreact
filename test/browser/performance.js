5178e30194b12dd8ebec3978cb9f3c6dbd381db2
zeromake <a390720046@gmail.com>
2017-09-23 20:42:34 +0800

    test: error

--- test/browser/performance.js
+++ test/browser/performance.js
@@ -2,7 +2,7 @@
 /*eslint no-console:0*/
 /** @jsx h */
 
-let { h, Component, render, buildVDom } = require(NODE_ENV==='production' ? '../../dist/zreact.min.js' : '../../build/zreact');
+let { h, Component, render, buildVDom } = require('../../build/zreact');
 
 const MULTIPLIER = ENABLE_PERFORMANCE ? (coverage ? 5 : 1) : 999999;
 
