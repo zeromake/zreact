bb147800ff81968e7809296860bccb3b5b46870a
zeromake <a390720046@gmail.com>
2018-11-04 20:07:04 +0800

    feat: add dom render

---
+++ src/render/dom/find-dom-node.ts
@@ -0,0 +1,15 @@
+import { findHostInstance } from "../../fiber/find-host-instance";
+
+// [Top API] ReactDOM.findDOMNode
+export function findDOMNode(fiber: any) {
+    if (fiber == null) {
+        return null;
+    }
+    if (fiber.nodeType === 1) {
+        return fiber;
+    }
+    if (!fiber.render) {
+        throw TypeError("findDOMNode:invalid type");
+    }
+    return findHostInstance(fiber);
+}
